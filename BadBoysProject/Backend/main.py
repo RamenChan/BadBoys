b5951dac7d5920dda38f51b137b06f8152b8f57f
Mehmet KOÇ <128498523+Sjokle@users.noreply.github.com>
2025-11-18 21:11:19 +0300

    Backend Frontend geliştirmeleri
    Backend;
    -api.py dosyası eklendi bu dosyadaki get_data_by_api hacker-news.firebaseio.com sitesinin datalarına ulaşıp top 20 haberi mongo db ye çeker.
    -api.py dosyası içerisindeki get_stories çekilmiş ve mongodb ye kaydedilmiş 20 datayı main.py den gelen istek doğrultusunda iletir.
    -threat mantığı sayesinde 5 dk da bir tekrardan istek atarak güncel datayı insert eder.
    -websocket server sayesinde istek atıldıktan sonra forntende bilgi vererek istek atılmasını sağlar
    Forntend;
    -isteği atılan datalar için uygun alan dashboard.jsx dosyasına eklendi.
    -bir sonraki adım olan HTML datadan bilgi almak için alan oluşturuldu.

--- BadBoysProject/Backend/main.py
+++ BadBoysProject/Backend/main.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO
 from sezarV2 import to_hash  
 from user_enterance import user_exists, user_add_check
 from core import validate_payload
 from system_utilities import ResultCode, system_handshake
 from key_rotation import rotate_master_key
+from services.api import get_stories, get_data_by_api
 import threading
 import time
-import subprocess 
+
+
 
 app = Flask(__name__)
 CORS(app) 
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
 
 @app.route("/user_check", methods=["POST"])
 def encrypt():
@@ -54,8 +60,40 @@ def key_rotation_scheduler():
             system_handshake(ResultCode.ERROR, error_message=str(e), function_name='main/key_rotation_scheduler')
         time.sleep(600)
 
-if __name__ == "__main__":
-    scheduler_thread = threading.Thread(target=key_rotation_scheduler, daemon=True)
-    scheduler_thread.start()
 
-    app.run(debug=False, use_reloader=False, port=8000)
+@app.route("/api/stories", methods=["GET"])
+def api_get_stories():
+    result = get_stories()
+    return jsonify({"result": result})
+
+
+
+def api_fetch_scheduler():
+    while True:
+        print("[API_DATA] API veri çekimi başlatılıyor...")
+        try:
+            get_data_by_api()   
+            print("[API_DATA] API veri çekimi başarıyla tamamlandı.")
+
+
+            # Yeni veri çekildiğinde frontend'e gönder
+            latest = get_stories()
+            socketio.emit("new_stories", latest)  # "new_stories" event’i
+
+
+
+        except Exception as e:
+            print("[API_DATA] API veri çekim hatası. DB kayıt atıldı.")
+            system_handshake(ResultCode.ERROR, error_message=str(e), function_name="main/api_fetch_scheduler")
+        time.sleep(600)
+
+
+
+
+
+if __name__ == "__main__":
+    threading.Thread(target=key_rotation_scheduler, daemon=True).start()
+
+    threading.Thread(target=api_fetch_scheduler, daemon=True).start()
+
+    app.run(debug=True, use_reloader=False, port=8000)
